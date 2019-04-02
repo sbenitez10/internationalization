c89e2d9c6db8a39690db68783c59c7dcbfc8e2aa
Sebastian Benitez Diaz <sebastianbd95@gmail.com>
2019-04-02 09:16:23 -0500

    retos realizados

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {FormattedDate} from 'react-intl';
+import {FormattedRelative} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
+import{FormattedNumber} from 'react-intl';
 
 export default class Job extends React.Component {
 
@@ -8,9 +12,26 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td> {this.props.offer.salary}
+					  &emsp;<FormattedPlural
+					   value = {this.props.offer.salary}
+					   one='million'
+					   other='millions'
+					  />
+				  </td>
+
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+					
+					<FormattedDate
+						value={new Date(this.props.offer.date)}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/>&emsp;<FormattedRelative value={new Date(this.props.offer.date)}/>
+				</td>
+				<td><FormattedNumber value={this.props.offer.views}/>&emsp;views</td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider, addLocaleData} from 'react-intl';
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData(esLocaleData);
+var userLang= navigator.language;
+
+function getLocale(){
+    return userLang === "es" ? localeEsMessages : localeEnMessages
+}
+
+ReactDOM.render(
+	<IntlProvider locale={userLang} messages= {getLocale()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
+
